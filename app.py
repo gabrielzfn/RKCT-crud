93bd40f76775c8d5973f34f2aa456365fd03f489
Gabriel Zefino <gabrielzt12@hotmail.com>
2024-09-26 20:14:42 -0300

    create R of CRUD

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+# __name__ sendo executado como mais quando é feito de forma manual
+app = Flask(__name__)
+
+# No momento utilizando uma lista pois não tem um banco de dados
+task = []
+taskid = 1
+
+
+# Create:
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global taskid
+    data = request.get_json()
+    new_task = Task(id=taskid, title=data['title'], description=data.get('description', ""))
+    taskid += 1
+    task.append(new_task)
+    return jsonify({"Message": "New task created"})
+
+
+# Get (todos):
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in Task]
+    output = {
+        "tasks": task_list,
+        "total": len(task_list)
+    }
+    return jsonify(output)
+
+
+# Get (apenas o primeiro):
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in task:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"Message": "Não foi possível encontrar a tarefa."}), 404
+
+
+
+# app.run é utilizado para iniciar, rodar a aplicação
+# A propriedade debug é utilizado para mostrar as funcionalidades do servidor web (logs)
+# **** Nunca utilizar essa forma para fornecer a clientes ****
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/task.py
@@ -0,0 +1,17 @@
+class Task:
+    def __init__(self, id, title, description, completed=False) -> None:
+        self.id = id
+        self.title = title
+        self.description = description
+        self.completed = completed
+
+    
+    # Retorna as informações em dicionário
+    def to_dict(self):
+        return 
+        {
+            "id"            : self.id,
+            "title"         : self.title,
+            "description"   : self.description,
+            "completed"     : self.completed
+        }

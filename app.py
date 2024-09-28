35ea483c4ca7a17afad1feacbf2bf0fafb350973
Gabriel Zefino <gabrielzt12@hotmail.com>
2024-09-28 18:09:11 -0300

    add Update (put) method

--- app.py
+++ app.py
@@ -4,40 +4,63 @@ from models.task import Task
 app = Flask(__name__)
 
 # Utilizando uma lista no lugar de um banco de dados
-task = []
+tasks = []
 taskid = 1
 
 
 # Create:
-@app.route('/tasks', methods=['POST'])
+@app.route('/tasks', methods=["POST"])
 def create_task():
     global taskid
     data = request.get_json()
     new_task = Task(id=taskid, title=data['title'], description=data.get('description', ""))
     taskid += 1
-    task.append(new_task)
+    tasks.append(new_task)
+    print(tasks)
     return jsonify({"Message": "Nova tarefa criada."})
 
 
-# Get (todos):
-@app.route('/tasks', methods=['GET'])
+# Get (todos os itens da lista):
+@app.route('/tasks', methods=["GET"])
 def get_tasks():
-    task_list = [task.to_dict() for task in Task]
+    task_list = [task.to_dict() for task in tasks]
+
     output = {
-        "tasks": task_list,
-        "total": len(task_list)
-    }
+                "tasks": task_list,
+                "total_tasks": len(task_list)
+            }
     return jsonify(output)
 
 
-# Get (apenas o primeiro):
-@app.route('/tasks/<int:id>', methods=['GET'])
+# Get (apenas o primeiro da lista):
+@app.route('/tasks/<int:id>', methods=["GET"])
 def get_task(id):
-    for t in task:
+    for t in tasks:
         if t.id == id:
             return jsonify(t.to_dict())
         
     return jsonify({"Message": "Não foi possível encontrar a tarefa."}), 404
+
+
+# Put
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def updatet(id):
+    task = None
+    for t in task:
+        if t.id == id:
+            task = t
+    print(task)
+
+    if task == None:
+        return jsonify({"Message": "Não foi possível encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    
+    return jsonify({"Message": "Tarefa atualizada com sucesso"})
 
 
 
